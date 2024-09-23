875ed1df9e033d98611a97f45074b0a173eebbff
R-y-a-n8 <ryan.kinuthia@student.moringaschool.com>
2024-09-23 04:12:41 +0300

    prop

--- lib/employee.py
+++ lib/employee.py
@@ -2,7 +2,6 @@
 from __init__ import CURSOR, CONN
 from department import Department
 
-
 class Employee:
 
     # Dictionary of objects saved to the database.
@@ -97,8 +96,10 @@ class Employee:
     @classmethod
     def instance_from_db(cls, row):
         """Return an Employee object having the attribute values from the table row."""
+        if row is None:
+            return None  # Handle missing row case.
 
-        # Check the dictionary for  existing instance using the row's primary key
+        # Check the dictionary for an existing instance using the row's primary key
         employee = cls.all.get(row[0])
         if employee:
             # ensure attributes match row values in case local instance was modified
@@ -107,8 +108,7 @@ class Employee:
             employee.department_id = row[3]
         else:
             # not in dictionary, create new instance and add to dictionary
-            employee = cls(row[1], row[2], row[3])
-            employee.id = row[0]
+            employee = cls(row[1], row[2], row[3], row[0])
             cls.all[employee.id] = employee
         return employee
 
@@ -142,7 +142,7 @@ class Employee:
         sql = """
             SELECT *
             FROM employees
-            WHERE name is ?
+            WHERE name = ?
         """
 
         row = CURSOR.execute(sql, (name,)).fetchone()
